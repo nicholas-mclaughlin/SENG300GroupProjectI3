74df230701d7780397246d4e65e0139c9d4995a4
maggie1126 <maggie.ma1@ucalgary.ca>
2021-04-09 23:53:32 -0600

    More attendant test case added

--- SelfCheckoutSystem - Software - Test/src/org/lsmr/test/AttendentControlConsoleTest.java
+++ SelfCheckoutSystem - Software - Test/src/org/lsmr/test/AttendentControlConsoleTest.java
@@ -7,19 +7,29 @@ import java.util.Currency;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.PriceLookupCode;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.products.BarcodedProduct;
+import org.lsmr.selfcheckout.products.PLUCodedProduct;
 import org.lsmr.selfcheckout.products.Product;
 import org.lsmr.software.AttendantControlConsole;
 import org.lsmr.software.AttendantDataBase;
+import org.lsmr.software.Database;
+import org.lsmr.software.DatabaseController;
+import org.lsmr.software.DatabaseItem;
+import org.lsmr.software.Purchase;
 import org.lsmr.software.SoftwareController;
 import org.lsmr.test.PaymentControllerTest.SoftwareControllerStub;
 
 public class AttendentControlConsoleTest {
 	private SelfCheckoutStation station;
 	private int[] banknoteDenominations = {5, 10, 20, 50, 100};
-	private AttendantDataBase attendantDataBase; //??
+//	private AttendantDataBase attendantDataBase; //??
 	private AttendantControlConsole attendantControlConsole;
 	private SoftwareControllerStub softwareControllerStub;
+//	private DatabaseController databaseController;
+//	private SoftwareController softwareController;
 
 	@Before
 	public void setUp() throws Exception {
@@ -34,6 +44,7 @@ public class AttendentControlConsoleTest {
 
         station = new SelfCheckoutStation(Currency.getInstance("CAD"), banknoteDenominations, coinDenominations, 300, 1);
         softwareControllerStub = new SoftwareControllerStub(station);
+        //softwareController = new SoftwareController(station);
         
         attendantControlConsole = new AttendantControlConsole(softwareControllerStub, station);
 	}
@@ -95,4 +106,88 @@ public class AttendentControlConsoleTest {
 		
 		assertEquals(expected, result);
 	}
+	
+	// Attendant looks up product
+	// Test attendant looks up barcoded item
+	@Test
+	public void testLookUpBarcodedProduct(){
+		attendantControlConsole.attendantDataBase.addEntry("employee1", "1234");
+		attendantControlConsole.logIn("employee1", "1234");
+        Barcode code = new Barcode("12345");
+        BarcodedProduct product = new BarcodedProduct(code, "item1", new BigDecimal("10.00"));
+        DatabaseItem item = new DatabaseItem(product, 50);
+        Database.BARCODED_PRODUCT_DATABASE.put(code, item);
+		
+		Product result = attendantControlConsole.attendantLookforBarcodedProdcut(code);
+		
+		assertEquals(product, result);
+	}
+	
+	// Test attendant looks up PLU coded product
+	@Test
+	public void testLookUpPLUCode(){
+		attendantControlConsole.attendantDataBase.addEntry("employee1", "1234");
+		attendantControlConsole.logIn("employee1", "1234");
+        PriceLookupCode code = new PriceLookupCode("0987");
+        PLUCodedProduct product = new PLUCodedProduct(code, "item1", new BigDecimal("10.00"));
+        Database.PLU_PRODUCT_DATABASE.put(code, product);
+		
+		Product result = attendantControlConsole.attendantLookforPLUcodedProdcut(code);
+		
+		assertEquals(product, result);
+	}
+	
+	// Attendant removes product
+	@Test
+	public void testRemoveProduct() {
+		attendantControlConsole.attendantDataBase.addEntry("employee1", "1234");
+		attendantControlConsole.logIn("employee1", "1234");
+        Barcode code = new Barcode("12345");
+        BarcodedProduct product = new BarcodedProduct(code, "item1", new BigDecimal("10.00"));
+       
+        Purchase purchase = new Purchase();
+        purchase.addItem(product);
+        attendantControlConsole.removeProdcutFromorder(purchase, product);
+        
+        assertEquals(new BigDecimal("0"), purchase.getSubtotal());
+	}
+	
+	// Attendant adds paper
+	@Test
+	public void testAddPaper() {
+		
+	}
+	
+	@Test
+	public void testEmptyBanknote() {
+		
+	}
+
+	// Attendant stats up station
+	@Test
+	public void testStartUp() {
+		attendantControlConsole.attendantDataBase.addEntry("employee1", "1234");
+		attendantControlConsole.logIn("employee1", "1234");
+		boolean expected = true;
+		
+		attendantControlConsole.startUpStation();
+		
+		assertEquals(expected,attendantControlConsole.getStationStatus());
+	}
+	
+	// Attendant shuts down station
+	@Test
+	public void testShutDown() {
+		attendantControlConsole.attendantDataBase.addEntry("employee1", "1234");
+		attendantControlConsole.logIn("employee1", "1234");
+		boolean expected = false;
+		
+		attendantControlConsole.shutDownStation();
+		
+		assertEquals(expected,attendantControlConsole.getStationStatus());
+	}
+	
+	
+	
+	
 }

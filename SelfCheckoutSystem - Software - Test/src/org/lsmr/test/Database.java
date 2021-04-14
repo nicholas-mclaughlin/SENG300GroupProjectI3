2d06b74a1a16b6ed13bd9ebdc3d18c757bb0b5fd
Sysimq <52726283+Sysimq@users.noreply.github.com>
2021-04-14 16:25:11 -0600

    Add files via upload

---
+++ SelfCheckoutSystem - Software - Test/src/org/lsmr/test/Database.java
@@ -0,0 +1,64 @@
+package org.lsmr.software;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.PriceLookupCode;
+import org.lsmr.selfcheckout.external.CardIssuer;
+import org.lsmr.selfcheckout.products.PLUCodedProduct;
+import org.lsmr.selfcheckout.products.Product;
+
+/**
+ * This database is provided for testing purposes,
+ * and is very similar to the ProductDatabase
+ * with a few changes to make it support expected weights.
+ * We will replace this database with whatever database
+ * is provided/specified in future iterations.
+ */
+public class Database {
+    private static Database database = new Database();
+
+    public static Database getDatabase() {
+        return database;
+    }
+
+    /**
+     * Instances of this class are not needed, so the constructor is private.
+     */
+    private Database() {}
+
+    /**
+     * The known barcoded products, indexed by barcode.
+     */
+    public static final Map<Barcode, DatabaseItem> BARCODED_PRODUCT_DATABASE = new HashMap<>();
+
+    // Armeen's method
+    public static final Map<PriceLookupCode, DatabaseItem> PLU_PRODUCT_DATABASE = new HashMap<>();
+    
+    /**
+     * The know PLU products, indexed by barcode.
+     * Wei's method - added a 1 to the name
+     */
+    public static final Map<PriceLookupCode, PLUCodedProduct> PLU_PRODUCT_DATABASE1 = new HashMap<>();
+
+    /**
+     * A count of the items of the given product that are known to exist in the
+     * store. Of course, this does not account for stolen items or items that were
+     * not correctly recorded, but it helps management to track inventory.
+     */
+    public static final Map<Product, Integer> INVENTORY = new HashMap<>();
+    
+
+    public static final Map<String, CardIssuer> BANK_CARD_DATABASE = new HashMap<>();
+    
+
+    /*
+     * Database for giftCard
+     */
+    
+    public static final Map<String, BigDecimal> GIFT_CARD_DATABASE = new HashMap<>();
+    
+}
